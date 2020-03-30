da7f1a5d07ee01476c32a10a12c506f09f03d40e
Robert Pirtle <astropirtle@gmail.com>
2020-03-30 12:36:34 -0700

    add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# this will unlink all home files except for the ~/.dotfiles directory link
+
+echo running cleanup script
+
+function remove_link() {
+  if [ -L $1 ]; then
+    unlink $1
+    echo '  unlinked' $1
+  fi
+}
+
+
+FILES='
+.aliases
+.bash_profile
+.bash_prompt
+.gitconfig
+'
+for FILE in $FILES; do remove_link $FILE; done
+
+echo cleanup completed
